36a7e80383111e9a98e130ec759cc32e7d206309
haggar39 <hisseinbaharfadoul@email.com>
2025-10-21 22:32:21 +0000

    min jeu

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+ruby '3.3.0'
+gem 'pry'
+gem 'rubocop', '~> 1.81.1'
---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'bundler/setup'
+Bundler.require
+
+require_relative 'lib/player'
+require_relative 'lib/game'
